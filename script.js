a2d890b723bf252e11d850ffddf56afb96825322
Miguel <miguelzapatauh@gmail.com>
2026-05-17 14:40:51 -0600

    Restore portfolio files

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Mediveins AI portfolio loaded successfully");
+
+/*
+  Future improvements:
+
+  - animations
+  - visual effects
+  - API integrations
+  - dynamic screenshots
+  - live demos
+  - dark mode
+*/
